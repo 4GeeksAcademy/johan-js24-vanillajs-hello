b6c81d30affd9d8d4cd6332f76d6e2dfee59e170
Johan Andres Cardenas Hernandez <tecnogas2010@gmail.com>
2026-03-14 16:52:24 +0000

    Creador de Excusa

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function () {
-}  let who = ['The baby', 'My coche', 'My work'];
-let action = ['irse'];
-let what = ['the car'];
- }
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed on", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+        "before the class",
+        "when I was sleeping",
+        "while I was exercising",
+        "during my lunch",
+        "while I was praying"
+    ];
+
+    function getRandomElement(array) {
+        let randomIndex = Math.floor(Math.random() * array.length);
+        return array[randomIndex];
+    }
+
+    let excuse =
+        getRandomElement(who) +
+        " " +
+        getRandomElement(action) +
+        " " +
+        getRandomElement(what) +
+        " " +
+        getRandomElement(when);
+
+    document.querySelector("#excuse").innerHTML = excuse;
+};
